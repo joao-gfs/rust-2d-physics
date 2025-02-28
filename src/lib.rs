0821fdfcaf9934c20138e3df8cc64db6ddd9eaa3
joao-gfs <joaoguilhermeferreira19@gmail.com>
2025-02-27 23:08:40 -0300

    update and apply force functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,35 @@
+use vec2d::Vec2;
+
 mod vec2d;
 
 pub struct Body {
     position: vec2d::Vec2,
     velocity: vec2d::Vec2,
     acceleration: vec2d::Vec2,
-    radius: f32
+    mass: f32,
+    radius: f32,
 }
 
 impl Body {
-    fn new(x: f32, y: f32, vx: f32, vy: f32, ax: f32, ay: f32, r: f32) -> Self {
+    fn new(x: f32, y: f32, vx: f32, vy: f32, m: f32, r: f32) -> Self {
         Body {
             position: vec2d::Vec2::new(x, y),
             velocity: vec2d::Vec2::new(vx, vy),
-            acceleration: vec2d::Vec2::new(ax, ay),
+            acceleration: vec2d::Vec2::new(0.0, 0.0),
+            mass: m,
             radius: r,
         }
+    }
+
+    fn apply_force(&mut self, force: f32) {
+        let acceleration = force / self.mass;
+        self.acceleration += acceleration;
+    }
+
+    fn update(&mut self, delta_t: f32) {
+        self.velocity += self.acceleration * delta_t;
+        self.position += self.velocity * delta_t;
+        self.acceleration = Vec2::new(0.0, 0.0);
     }
 }
 
